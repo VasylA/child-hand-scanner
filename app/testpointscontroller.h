44152bc298ad26e037495ee3d0367a0c375df66f
Vasyl <Vasyl18@ukr.net>
2016-11-01 12:09:51 +0200

    Added files for interaction with GPIO pins.

---
+++ app/testpointscontroller.h
@@ -0,0 +1,41 @@
+#ifndef TESTPOINTSCONTROLLER_H
+#define TESTPOINTSCONTROLLER_H
+
+#include "gpio_class.h"
+
+#include <QObject>
+
+class TestpointsController : public QObject
+{
+    Q_OBJECT
+
+public:
+    TestpointsController(QObject *parent = 0);
+    ~TestpointsController();
+
+public slots:
+    void checkInGpioStatus();
+    void resetOutGpiosStatus();
+    void sendPuzzleCompeteSignalToOutGpios();
+    void sendPuzzleIncompeteSignalToOutGpios();
+
+signals:
+    void initialAppStateRequested();
+    void laserPassed();
+    void laserFailed();
+
+private:
+    void initTestpoints();
+
+private:
+    GPIOClass _gpio4;
+    GPIOClass _gpio5;
+    GPIOClass _gpio6;
+    GPIOClass _gpio17;
+
+//    QTimer _statusCheckTimer;
+
+    static const int STATUS_CHECK_PERIOD = 300;
+};
+
+#endif // TESTPOINTSCONTROLLER_H
